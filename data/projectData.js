e8dfdab76fb4ac4bb525893729cc49908fe55df1
KSJIM <126589490+KSJIM@users.noreply.github.com>
2025-04-17 00:11:32 -0600

    caltyp更新wip

--- data/projectData.js
+++ data/projectData.js
@@ -74,24 +74,23 @@ sections: [
         info: [
           { label: 'Project', value: 'Caltyp' },
           { label: 'Role', value: 'Artist / Coder' },
-          { label: 'Year', value: '2025' },
+          { label: 'Year', value: 'Work in progress' },
           { label: 'Advisor', value: 'Mike Rivera' },
               { label: 'Introduction', value: <p className="tight-value">
-        Creating generative art based on the aesthetics of Chinese calligraphy and landscape painting,
-        using Turtle graphics and L-system
+       Caltyp explores the generative reconstruction of Latin letterforms through dynamic simulation in Kangaroo2 (Grasshopper). The outcomes are transformed into 3D compositions, which serve as 'stamp' models for collaborative artistic creation with users.
         </p> }
   ],
       
         
   sections: [
       {type: 'imageGroup',
-          layout: 'carousel',
-          images: ['/contents/L-shufa/i1.webp', '/contents/L-shufa/i2.webp','/contents/L-shufa/i3.webp','/contents/L-shufa/i4.webp']
+          layout: 'single',
+          images: ['/contents/Caltyp/temp1.webp']
         },
   
   
         
-      {
+      /*{
         label: 'INSPIRATION',
         content: 'This project is inspired by the poetic imagery of traditional Chinese landscape painting and the abstract composition of flowing curves.',
       },
@@ -121,7 +120,7 @@ sections: [
       {
           label: 'CREATION',
           content: 'In the final stage, I simulated the effect of a “bamboo forest” by generating patterns through dual-directional drawing, incorporating randomness in both stroke thickness and intensity.',
-        }
+        }*/
     ],
         
   
