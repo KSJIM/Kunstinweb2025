87523fde7ae8ec3f3f58f28ea16b7ba8a92bd2c0
KSJIM <126589490+KSJIM@users.noreply.github.com>
2025-05-26 16:31:50 +0800

    resume

--- pages/index.js
+++ pages/index.js
@@ -470,7 +470,7 @@ activeColor;
       I’m also currently enrolled in the MS in Human-Computer Interaction at the ATLAS Institute. 
       
       <a
-        href="/Resume/Resume-YekunXu.pdf"
+        href="/Resume/Resume-YekunXu2025.6.pdf"
         target="_blank"
         rel="noopener noreferrer"
         className="resume-link"
